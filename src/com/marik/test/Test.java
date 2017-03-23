1babd4261542fb54a7e976a0bbdfaf07662d1ebf
lingb <lingb@LINGB-PC.500wan.com>
2017-03-23 20:34:05 +0800

    update

--- src/com/marik/test/Test.java
+++ src/com/marik/test/Test.java
@@ -11,8 +11,6 @@ public class Test {
 	public static void main(String[] args) throws Exception {
 		 ELF.dlopen("C:\\Users\\monitor\\Desktop\\test");
 
-		// new ELF("C:\\Users\\monitor\\Desktop\\Decomplied
-		// File\\500彩票\\lib\\armeabi\\libesunlib.so");
 		// ELF.decode("C:\\Users\\monitor\\Desktop\\Decomplied
 		// File\\crackme\\lib\\armeabi\\libdata.so");
 	}
